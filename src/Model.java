dc15811b28f947534e97640a4ae0e60f3f9ee066
iamiluxa <iamiluxa@gmail.com>
2020-02-09 22:41:02 +0200

    Model generate

--- src/Model.java
+++ src/Model.java
@@ -1,12 +1,27 @@
+import java.util.Random;
 
 /**
  * Created by User on 09.02.2020.
  */
 public class Model {
-    private static final int MIN_VALUE_INIT = 0;
+    private static final int MIN_VALUE_INIT = 1;
     private static final int MAX_VALUE_INIT = 100;
 
+    private int rand( ) {
+        Random random = new Random();
+        return random.nextInt(MAX_VALUE_INIT - MIN_VALUE_INIT) + MIN_VALUE_INIT;
+    }
+
+    private int rand(int minRangeValue, int maxRangeValue) {
+        Random random = new Random();
+        return random.nextInt(maxRangeValue - minRangeValue) + minRangeValue;
+    }
+
     // The Program logic
+
+    public int getRandomNumber() {
+        return rand();
+    }
 
 
 }
